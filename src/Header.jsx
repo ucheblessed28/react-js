dcdf6a40d6077889ec81e57a34c75bef8af6815b
Uche Blessed <ucheblessed1@gmail.com>
2025-01-10 11:19:41 +0100

    added files

---
+++ src/Header.jsx
@@ -0,0 +1,12 @@
+// functional components
+
+function Header() {
+    return (
+        <header>
+            <h1>Welcome to React + Vite</h1>
+            <p>This is your first app!</p>
+        </header>
+    );
+}
+
+export default Header;
